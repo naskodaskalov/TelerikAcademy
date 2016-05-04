8b36355bf7969ce3f75026ae952e1b1b37f46a85
naskodaskalov <nasko.daskalov@abv.bg>
2016-05-04 12:47:32 +0300

    Added files via upload

---
+++ C# Advanced Homeworks/01. Arrays/07. Selection sort.cs
@@ -0,0 +1,31 @@
+//Sorting an array means to arrange its elements in increasing order. Write a program to sort an array. Use the Selection sort algorithm: Find the smallest element, move it at the first position, find the smallest from the rest, move it at the second position, etc.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class SelectionSort
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(arr);
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+        }
+    }
+}
